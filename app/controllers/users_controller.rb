00351a007a46fbd3f3bdcdb0313805b2f955c716
DigitalMoon35 <freshlife35@yahoo.com>
2022-11-26 18:33:56 -0500

    added users#create action in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      token = encode_token({user_id: @user.id})
+      render json: {user: @user, token: token }, status: :ok
+    else
+      render json: {error: "Invalid username or password"}, status: :unprocessable_entity
+    end
+  end
   def index
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :password)
+    end
 end
